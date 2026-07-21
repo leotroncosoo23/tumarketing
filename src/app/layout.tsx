36e11f4a467784da3705d0600c1e628642b8c046
Leonardo Troncoso <troncosoleouni@gmail.com>
2026-07-21 09:17:33 -0300

    aumento de velocidad

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,6 +28,10 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased scroll-smooth`}
     >
+      <head>
+        <link rel="preconnect" href={process.env.NEXT_PUBLIC_SUPABASE_URL} />
+        <link rel="dns-prefetch" href={process.env.NEXT_PUBLIC_SUPABASE_URL} />
+      </head>
       <body className="min-h-full flex flex-col">
         <Providers>{children}</Providers>
       </body>
